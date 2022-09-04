18a875e5bb8af0bddc522d97a86e144fb41555f4
Manic Chuang <manic.chuang@gmail.com>
2022-09-05 00:47:24 +0900

    feat: sort the wrappers with alphabet order
    Easier for users to find their wrappers.

--- src/WrapperCreatorModal.ts
+++ src/WrapperCreatorModal.ts
@@ -13,7 +13,7 @@ export default class WrapperCreatorModal extends Modal {
 			this.wrapper = wrapper;
 			this.editMode = true;
 		} else {
-			this.wrapper = { name: '', startTag: '', endTag: '' };
+			this.wrapper = { id: `w${new Date().getTime()}`, name: '', startTag: '', endTag: '' };
 			this.editMode = false;
 		}
 	}
@@ -33,7 +33,6 @@ export default class WrapperCreatorModal extends Modal {
 			.setDesc("Specify the Name of your wrapper.")
 			.addText(cb => {
 				cb.setValue(this.wrapper.name ?? "")
-					.setDisabled(this.editMode)
 					.onChange(value => {
 						this.wrapper.name = value.trim();
 					})
